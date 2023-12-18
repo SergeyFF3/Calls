3bd535801591b4e9163e740c98cba01871d64570
SergeyFF3 <sergey@redrazr.com>
2023-12-18 18:27:42 +0300

    added scss, Table component

---
+++ src/components/Table/Table.tsx
@@ -0,0 +1,26 @@
+export const Table = () => (
+  <table className="app__table">
+    <thead>
+      <tr>
+        <th>Тип</th>
+        <th>Время</th>
+        <th>Сотрудник</th>
+        <th>Звонок</th>
+        <th>Источник</th>
+        <th>Оценка</th>
+        <th>Длительность</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+    </tbody>
+  </table>
+);
